d27ac365012991b87cdcc584c21d95df2ce926c8
snsaleh1 <snsaleh11@gmail.com>
2019-08-05 13:33:45 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var sum =0;
+var displayEl=document.querySelector('.sumDifference');
+var inputEl=document.getElementById('input');
+
+document.getElementById('plus').addEventListener('click', function(){
+    sum+= parseFloat(inputEl.value);
+    render();
+})
+
+document.getElementById('minus').addEventListener('click', function(){
+    sum-= parseFloat(inputEl.value);
+    render();
+})
+
+function init() {
+    sum;
+    render();
+    if(sum < 0, function(){
+        document.querySelector('.sumDifference').innerHTML.style.color = 'red';
+    })
+}
+
+function render() {
+    displayEl.innerHTML = (sum);
+}
+
+document.addEventListener('hover', function(button){
+    event.target.style.color= 'gray';
+});
